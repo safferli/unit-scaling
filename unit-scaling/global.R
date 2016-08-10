d1e1859c3dec9749654645867d29482f69940190
Christoph Safferling <christoph.safferling@bluebyte.de>
2016-08-10 12:02:14 +0200

    storing conversions and outputting to DT

--- unit-scaling/global.R
+++ unit-scaling/global.R
@@ -14,20 +14,3 @@ l.units <- c("mm", "cm", "m", "inch", "ft", "yd")
 from.units <- l.units
 to.units <- l.units
 
-# dta <- data.frame(
-#   from_unit = vector("character", length = 1),
-#   to_unit = vector("character", length = 1),
-#   scale_factor = vector("character", length = 1), 
-#   from_length = vector("numeric", length = 1), 
-#   to_length = vector("numeric", length = 1),
-#   stringsAsFactors = FALSE
-# )
-
-dta <- data.frame(
-  from_unit = NULL,
-  to_unit = NULL,
-  scale_factor = NULL, 
-  from_length = NULL, 
-  to_length = NULL,
-  stringsAsFactors = FALSE
-)
--- unit-scaling/server.R
+++ unit-scaling/server.R
@@ -14,21 +14,40 @@ server <- function(input, output) {
     )
   })
   
+  # render DT of the stored conversions
   output$datatable <- DT::renderDataTable(
-    dta, options = list(
+    {values$dta}, options = list(
       dom = 'Bfrtip', buttons = c('copy', 'csv', 'pdf', 'print')
     ), extensions = "Buttons", rownames = FALSE
   )
-  
-  observeEvent(input$keep, {
-    keep.dta <- data.frame(
-      from_unit = input$from_unit, 
-      to_unit = input$to_unit, 
-      scale_factor = paste0(input$scale_numerator, "/", input$scale_denominator),
-      from_length = input$from_length,
-      to_length = measurements::conv_unit(input$from_length, from = input$from_unit, to = input$to_unit)*input$scale_numerator/input$scale_denominator,
+
+  # reative dataframe: http://stackoverflow.com/questions/23236944/add-values-to-a-reactive-table-in-shiny
+  values <- reactiveValues()
+  values$dta <- data.frame(
+      from_unit = vector("character", length = 0),
+      to_unit = vector("character", length = 0),
+      scale_factor = vector("character", length = 0),
+      from_length = vector("numeric", length = 0),
+      to_length = vector("numeric", length = 0),
       stringsAsFactors = FALSE
     )
-    dta <<- dplyr::bind_rows(dta, keep.dta)
+  
+  new.entry <- observe({
+    # button is pushed at least once
+    if(input$keep > 0) {
+      # isolate data we need
+      keep.dta <- isolate(
+        data.frame(
+          from_unit = input$from_unit,
+          to_unit = input$to_unit,
+          scale_factor = paste0(input$scale_numerator, "/", input$scale_denominator),
+          from_length = input$from_length,
+          to_length = measurements::conv_unit(input$from_length, from = input$from_unit, to = input$to_unit)*input$scale_numerator/input$scale_denominator
+        , stringsAsFactors = FALSE)
+      )
+      # bind data to existing
+      isolate(values$dta <- dplyr::bind_rows(values$dta, keep.dta))
+    }
   })
+  
 }
